05eb8f377586b00b24df0262b9f0d9a1f3770325
akshayamathamsetti <akshayamathamsetti@gmail.com>
2025-12-06 12:53:30 +0530

    Add files via upload

---
+++ tickets.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(4*x<=1000)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
+
